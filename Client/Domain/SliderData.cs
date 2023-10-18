99c503e50184ed88967a3ed43ba2474cfe29e738
Bohdan Botosh <bohdan.botosh@intenselab.com>
2023-10-18 21:17:16 +0300

    design: updated design, and projects structure

---
+++ Client/Domain/SliderData.cs
@@ -0,0 +1,24 @@
+﻿namespace Site.Client.Domain;
+
+public sealed record SliderData
+{
+    public SliderData()
+    {
+    }
+
+    public SliderData(string title, string subTitle, string description, string imageSrc)
+    {
+        Title = title;
+        SubTitle = subTitle;
+        Description = description;
+        ImageSrc = imageSrc;
+    }
+
+    public string Title { get; set; }
+
+    public string SubTitle { get; set; }
+
+    public string Description { get; set; }
+
+    public string ImageSrc { get; set; }
+}
